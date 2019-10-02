dae7b7be9d1add630a4bf8e411a6f098d545767d
Alice Freda <alicefreda@gmail.com>
2019-10-02 22:45:57 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,9 @@
-# Your code here
-def meal_choice(veg1, veg2, protein = "meat")
+def order(veg1, veg2, protein = "meat")
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+def meal_choice
   p "What a nutritious meal!"
   puts order
 end
-order = "A plate of #{protein} with #{veg1} and #{veg2}."
+
 meal_choice("broccoli", "macaroni", "tofu")
